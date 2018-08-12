c8cbd69736829e8549eb83ac4c731ad55f7ddf74
sushi <joe@ecotonetech.com>
2018-08-12 17:06:37 -0500

    changed const to let

--- src/controllers/comments.controller.js
+++ src/controllers/comments.controller.js
@@ -1,31 +1,41 @@
 class CommentsController {
   constructor() {
-    this.$addCommentForm = $('.add-comment')
+    this.$addCommentForm = $(".add-comment");
   }
 
   init() {
     // kick off controller from here
     this.addCommentFormListener();
   }
-
+  // `CommentsController.prototype.addCommentFormListener()`
+  //  Execute the render function on that found image object to append the new comment
   addCommentFormListener() {
-    // create comment form listener code here
-    const holder = this;
-    for (const index = 0; index < this.$addCommentForm.length; index++) {
-      this.$addCommentForm[index].addEventListener('submit', function
-      (event) {
+
+    // Create this to be in self, because self is only used in addCommentFormListener() scope which will reference the correct moment of "this" via an alias.
+    let self = this;
+    //  Iterates through each comment form and adds an eventlistener to trigger a function on form submit
+    for (let index = 0; index < this.$addCommentForm.length; index++) {
+      this.$addCommentForm[index].addEventListener("submit", function(event) {
+        //Added to prevent the page from reloading after you submit otherwise you lose the data.
         event.preventDefault();
-        const currentImageId = parseInt($(this).data('id'));
-        const commentContent = $(`#comment-description-${currentImageId}`).val();
-        const newComment = new Comment(commentContent, currentImageId)
-        holder.render(newComment)
-        $(`#comment-description-${currentImageId}`).val('');
-      })
+        //  Function should grab the imageId + comment and create a new Comment with those arguments
+        let currentImageId = parseInt($(this).data("id"));
+        let commentContent = $(
+          `#comment-description-${currentImageId}`
+        ).val();
+        let newComment = new Comment(commentContent, currentImageId);
+        self.render(newComment);
+        $(`#comment-description-${currentImageId}`).val("");
+      });
     }
   }
 
+  // `CommentsController.prototype.render(commentObject)`
+  // Selects the appropriate`ul` for this comment to be added to
+  // Appends the new comment element to this `ul`
   render(commentObject) {
-    $(`#comments-${commentObject.image.id}`).append
-    (commentObject.commentEl());
+    $(`#comments-${commentObject.image.id}`).append(commentObject.commentEl());
   }
 }
+
+
--- src/models/comment.js
+++ src/models/comment.js
@@ -1,29 +1,37 @@
 // create Comment class here
 'use strict'
 
+// `new Comment(comment, imageId)`
+// Should initialize with an id, image object(findImage) and commentContent(the actual text of the comment)
 class Comment {
-    constructor(commentContent, imageId) {
-        this.id = this.constructor.all.length;
-        this.commentContent = commentContent;
-        this.image = this.findImage(imageId);
-        this.constructor.all.push(this)
-    }
-
-    commentEl(){
+  constructor(commentContent, imageId) {
+    this.id = this.constructor.all.length;
+    this.commentContent = commentContent;
+    this.image = this.findImage(imageId);
+    this.constructor.all.push(this); // Should save new comment to Comment.all property
+  }
+ 
+  // `Comment.prototype.findImage(imageId)`
+  findImage(imageId) {
+    let activeImage = Image.all[imageId];
+      // Given an`int` for an image id, returns the image object with that id
+    let imageObj = {
+      id: this.id,
+      commentContent: this.commentContent
+    };
+    // Before return - adds current comment to image's comments property
+    activeImage.comments.push(imageObj);
+    return activeImage;
+  }
+    // `Comment.prototype.commentEl()`
+    //  Returns a string of html
+    //  Html has an`li` tag with an`id` field and shows the comment
+    commentEl() {
         return `<li class="comment" id=${this.id}">
         <p>${this.commentContent}</p>
         </li>`;
     }
-
-    findImage(imageId) {
-        let activeImage = Image.all[imageId]
-        let imageObj = {
-            id: this.id,
-            commentContent: this.commentContent
-        }
-        activeImage.comments.push(imageObj)
-        return activeImage;
-    }
 }
-
+// `Comment.all`
+// should return all of the comment objects in an array
 Comment.all = [];
